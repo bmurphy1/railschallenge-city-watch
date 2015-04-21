fdeeaa99a1e6dfc9e5d4d374c483be090de5ab69
Brian Murphy <bmurphy1@mac.com>
2015-04-21 14:49:31 -0500

    Add validations to Responder model

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,4 +1,8 @@
 class Responder < ActiveRecord::Base
   self.primary_key = 'name'
   self.inheritance_column = :_type_disabled
+
+  validates :name, presence: true, uniqueness: true
+  validates :type, presence: true
+  validates :capacity, presence: true, inclusion: (1..5).to_a
 end
