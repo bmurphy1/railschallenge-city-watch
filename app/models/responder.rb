c901e95e89030e9ae1a44e758b9393320d505c22
Brian Murphy <bmurphy1@mac.com>
2015-04-21 14:49:50 -0500

    Add custom #as_json to Responder model

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -5,4 +5,12 @@ class Responder < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :type, presence: true
   validates :capacity, presence: true, inclusion: (1..5).to_a
+
+  def as_json(options = nil)
+    { "responder" => {"emergency_code" => self.emergency_code,
+                  "type" => self.type,
+                  "name" => self.name,
+                  "capacity" => self.capacity,
+                  "on_duty" => self.on_duty } }
+  end
 end
