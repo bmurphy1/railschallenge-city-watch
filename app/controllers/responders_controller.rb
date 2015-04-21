fcc65949d487b07a3c44de732df9bb506768fffa
Brian Murphy <bmurphy1@mac.com>
2015-04-21 14:50:07 -0500

    Add remaining controller actions and strong params for RespondersController

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,5 +1,29 @@
 class RespondersController < ApplicationController
   before_action :page_not_found, only: [:new, :edit, :destroy]
+
+  def index
+    @responders = Responder.all
+
+    if @responders
+      render json: {responders: []}
+    else
+      render json: @responders
+    end
+  end
+
+  def create
+    @responder = Responder.new(responder_params)
+
+    if @responder.save
+      render json: @responder, status: :created
+    else
+      render json: { message: @responder.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+  end
+
   #Unused routes - new, edit, destroy
   def new
   end
@@ -11,6 +35,10 @@ class RespondersController < ApplicationController
   end
 
   private
+
+  def responder_params
+    params.require(:responder).except([:id]).permit([:name, :type, :capacity])
+  end
 
   def page_not_found
     render json: { message: 'page not found' } , status: 404
