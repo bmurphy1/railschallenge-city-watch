51f60800c099990e8ffad85bd3db0756ae3037f7
Brian Murphy <bmurphy1@mac.com>
2015-04-25 13:31:23 -0500

    Create index, show, create Emergency controller actions

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,5 +1,25 @@
 class EmergenciesController < ApplicationController
+  before_action :set_emergency, only: [:show, :update]
   before_action :page_not_found, only: [:new, :edit, :destroy]
+
+  def index
+    @emergencies = Emergency.all
+  end
+
+  def show
+    render json: @emergency
+  end
+
+  def create
+    @emergency = Emergency.new(emergency_params)
+
+    if @emergency.save
+      render "show.json", status: :created
+    else
+      render "errors.json", status: :unprocessable_entity
+    end
+  end
+
   def new
   end
 
@@ -10,6 +30,14 @@ class EmergenciesController < ApplicationController
   end
 
   private
+
+  def set_emergency
+    @emergency = Emergency.find(emergency_params[:code])
+  end
+
+  def emergency_params
+    params.require(:emergency).permit([:code, :fire_severity, :police_severity, :medical_severity])
+  end
 
   def page_not_found
     render json: { message: 'page not found' } , status: 404
